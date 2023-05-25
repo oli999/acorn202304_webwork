41ac9c735e1bcf1e55d4e240df4a8d8bb68ba043
kimgura <oli999@naver.com>
2023-05-25 15:55:59 +0900

    Step02 에 방명록 글 수정기능 구현함

--- Step02DataBase/src/main/java/test/guest/dao/GuestDao.java
+++ Step02DataBase/src/main/java/test/guest/dao/GuestDao.java
@@ -22,6 +22,87 @@ public class GuestDao {
 		}
 		return dao;
 	}
+	//글하나의 정보를 수정 반영하는 메소드(비밀번호가 일치해야 수정이 된다)
+	public boolean update(GuestDto dto) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int rowCount = 0;
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "UPDATE board_guest"
+					+ " SET writer=?, content=?"
+					+ " WHERE num=? AND pwd=?";
+			pstmt = conn.prepareStatement(sql);
+			//실행할 sql 문이 미완성이라면 여기서 완성
+			pstmt.setString(1, dto.getWriter());
+			pstmt.setString(2, dto.getContent());
+			pstmt.setInt(3, dto.getNum());
+			pstmt.setString(4, dto.getPwd());
+			//sql 문을 수행하고 변화된(추가, 수정, 삭제된) row 의 갯수 리턴 받기
+			rowCount = pstmt.executeUpdate();
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		//만일 변화된 row 의 갯수가 0 보다 크면 작업 성공
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	//글하나의 정보를 리턴하는 메소드
+	public GuestDto getData(int num) {
+		GuestDto dto=null;
+		//필요한 객체의 참조값을 담을 지역변수 미리 만들기
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//DbcpBean 객체를 이용해서 Connection 객체를 얻어온다(Connection Pool 에서 얻어오기)
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 
+			String sql = "SELECT num, writer, content, pwd, regdate"
+					+ " FROM board_guest"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문이 미완성이라면 여기서 완성
+			pstmt.setInt(1, num);
+			//select 문 수행하고 결과값 받아오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 에 담긴 내용 추출
+			while (rs.next()) {
+				dto=new GuestDto();
+				dto.setNum(num);
+				dto.setWriter(rs.getString("writer"));
+				dto.setContent(rs.getString("content"));
+				dto.setPwd(rs.getString("pwd"));
+				dto.setRegdate(rs.getString("regdate"));
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close(); //Connection 이 Connection Pool 에 반납된다.
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}
+	
 	//글 하나의 정보를 DB 에서 삭제하는 메소드
 	public boolean delete(GuestDto dto) {
 		Connection conn = null;
