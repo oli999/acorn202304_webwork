3fb0805f6bdb1d1a92c9bd7e0d9731388d209a3b
kimgura <oli999@naver.com>
2023-06-07 12:03:28 +0900

    Step05 에 파일 다운로드 구현함

--- Step05Final/src/main/java/test/file/dao/FileDao.java
+++ Step05Final/src/main/java/test/file/dao/FileDao.java
@@ -22,6 +22,52 @@ public class FileDao {
 		}
 		return dao;
 	}
+	//파일 하나의 정보를 리턴해주는 메소드
+	public FileDto getData(int num) {
+		FileDto dto=null;
+		//필요한 객체의 참조값을 담을 지역변수 미리 만들기
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//DbcpBean 객체를 이용해서 Connection 객체를 얻어온다(Connection Pool 에서 얻어오기)
+			conn = new DbcpBean().getConn();
+			//실행할 sql 문 
+			String sql = "SELECT writer, title, orgFileName, saveFileName, fileSize, regdate"
+					+ " FROM board_file"
+					+ " WHERE num=?";
+			pstmt = conn.prepareStatement(sql);
+			//sql 문이 미완성이라면 여기서 완성
+			pstmt.setInt(1, num);
+			//select 문 수행하고 결과값 받아오기
+			rs = pstmt.executeQuery();
+			//반복문 돌면서 ResultSet 에 담긴 내용 추출
+			while (rs.next()) {
+				dto=new FileDto();
+				dto.setNum(num);
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+				dto.setRegdate(rs.getString("regdate"));
+			}
+		} catch (SQLException se) {
+			se.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close(); //Connection 이 Connection Pool 에 반납된다.
+			} catch (Exception e) {
+			}
+		}
+		return dto;
+	}
+	
 	//파일 목록을 리턴하는 메소드 
 	public List<FileDto> getList(){
 		//파일 목록을 담을 ArrayList 객체 생성 
